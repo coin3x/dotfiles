76de901485f27c484c1963d5046f6c74706056df
Coin <cce5784@gmail.com>
2022-04-30 22:22:40 +0800

    誰會想要手動補引號啊

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,7 @@
 if status is-interactive
     set -Ux ADB_MDNS_OPENSCREEN 1
     set -Ux HOMEBREW_NO_ENV_HINTS 1
+    set -U fish_features ampersand-nobg-in-token qmark-noglob
 
     /opt/homebrew/bin/brew shellenv | source
     zoxide init fish | source
